aa281c41fabadd53877e9153a4f704c3edd09275
Kunai <lunarteclipse@gmail.com>
2020-06-11 11:44:49 +0100

    test commmit

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -7,7 +7,7 @@ public class ExampleMod implements ModInitializer {
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		// Proceed with mild caution. :)
 
 		System.out.println("Hello Fabric world!");
 	}
