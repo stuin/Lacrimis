6befce287dece50302e1050d262e0e8b0fcffd7f
Kunai <lunarteclipse@gmail.com>
2020-06-12 23:21:39 +0100

    tear lanterns kinda work now but theyre rlly overpowered

--- src/main/java/modfest/soulflame/block/TearLantern.java
+++ src/main/java/modfest/soulflame/block/TearLantern.java
@@ -1,17 +1,16 @@
 package modfest.soulflame.block;
 
-import modfest.soulflame.block.entity.InfusionTableEntity;
 import modfest.soulflame.block.entity.TearLanternEntity;
+import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.LanternBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.world.BlockView;
 
-public class TearLantern extends BlockWithEntity {
+public class TearLantern extends LanternBlock implements BlockEntityProvider {
     public TearLantern(Settings settings) {
         super(settings);
     }
-
     @Override
     public BlockEntity createBlockEntity(BlockView world) {
         return new TearLanternEntity();
--- src/main/java/modfest/soulflame/block/entity/TearLanternEntity.java
+++ src/main/java/modfest/soulflame/block/entity/TearLanternEntity.java
@@ -1,16 +1,14 @@
 package modfest.soulflame.block.entity;
 
 import modfest.soulflame.init.ModBlockEntityTypes;
-import net.minecraft.block.*;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.Tickable;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+
 
 public class TearLanternEntity extends BlockEntity implements Tickable {
-    private BlockPos pos;
-    private World world;
-    private BlockState state;
 
     public TearLanternEntity() {
         super(ModBlockEntityTypes.tearLantern);
@@ -20,17 +18,19 @@ public class TearLanternEntity extends BlockEntity implements Tickable {
     @Override
     public void tick() {
         int range = 6;
-        System.out.println(pos);
-        for (int x = -range; x <= range; x++)
-            for (int y = -range; y <= range; y++)
-                for (int z = -range; z <= range; z++) {
-                    BlockEntity table = world.getBlockEntity(pos.add(x, y-1, z));
-                    if (table instanceof CrucibleEntity) {
-                        CrucibleEntity casted = (CrucibleEntity) table;
-                        casted.addTears(5);
-                        System.out.println("Added 5 to " + pos);
-                        break;
+        BlockPos obsidianPos = this.pos.up(1);
+        BlockState obsidianState = this.world.getBlockState(obsidianPos);
+        if (obsidianState.getBlock() == Blocks.CRYING_OBSIDIAN) {
+            for (int x = -range; x <= range; x++)
+                for (int y = -range; y <= range; y++)
+                    for (int z = -range; z <= range; z++) {
+                        BlockEntity table = world.getBlockEntity(pos.add(x, y - 1, z));
+                        if (table instanceof CrucibleEntity) {
+                            CrucibleEntity casted = (CrucibleEntity) table;
+                            casted.addTears(2);
+                            break;
+                        }
                     }
-                }
+        }
     }
 }
