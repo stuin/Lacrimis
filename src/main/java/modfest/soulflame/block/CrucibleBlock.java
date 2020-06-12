519f523f24ca74a7111973d72a64ea0d6171ce7f
Kunai <lunarteclipse@gmail.com>
2020-06-12 13:32:53 +0100

    now able to get tears out of crucible with a glass bottle

--- src/main/java/modfest/soulflame/block/CrucibleBlock.java
+++ src/main/java/modfest/soulflame/block/CrucibleBlock.java
@@ -11,8 +11,10 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.function.BooleanBiFunction;
@@ -24,59 +26,82 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 public class CrucibleBlock extends LiquidTankBlock {
-	private static final VoxelShape RAY_TRACE_SHAPE = createCuboidShape(2.0D, 4.0D, 2.0D, 14.0D, 16.0D, 14.0D);
-	protected static final VoxelShape OUTLINE_SHAPE = VoxelShapes.combineAndSimplify(VoxelShapes.fullCube(), VoxelShapes.union(createCuboidShape(0.0D, 0.0D, 4.0D, 16.0D, 3.0D, 12.0D), createCuboidShape(4.0D, 0.0D, 0.0D, 12.0D, 3.0D, 16.0D), createCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 3.0D, 14.0D), RAY_TRACE_SHAPE), BooleanBiFunction.ONLY_FIRST);
+    private static final VoxelShape RAY_TRACE_SHAPE = createCuboidShape(2.0D, 4.0D, 2.0D, 14.0D, 16.0D, 14.0D);
+    protected static final VoxelShape OUTLINE_SHAPE = VoxelShapes.combineAndSimplify(VoxelShapes.fullCube(), VoxelShapes.union(createCuboidShape(0.0D, 0.0D, 4.0D, 16.0D, 3.0D, 12.0D), createCuboidShape(4.0D, 0.0D, 0.0D, 12.0D, 3.0D, 16.0D), createCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 3.0D, 14.0D), RAY_TRACE_SHAPE), BooleanBiFunction.ONLY_FIRST);
 
-	public CrucibleBlock(AbstractBlock.Settings settings) {
-		super(settings);
-	}
+    public CrucibleBlock(AbstractBlock.Settings settings) {
+        super(settings);
+    }
 
-	@Override
-	public BlockRenderType getRenderType(BlockState state) {
-		return BlockRenderType.MODEL;
-	}
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
 
-	@Override
-	public BlockEntity createBlockEntity(BlockView world) {
-		return new CrucibleEntity();
-	}
+    @Override
+    public BlockEntity createBlockEntity(BlockView world) {
+        return new CrucibleEntity();
+    }
 
-	@Override
-	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-		return OUTLINE_SHAPE;
-	}
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return OUTLINE_SHAPE;
+    }
 
-	@Override
-	public VoxelShape getRayTraceShape(BlockState state, BlockView world, BlockPos pos) {
-		return RAY_TRACE_SHAPE;
-	}
+    @Override
+    public VoxelShape getRayTraceShape(BlockState state, BlockView world, BlockPos pos) {
+        return RAY_TRACE_SHAPE;
+    }
 
-	@Override
-	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-		ItemStack stack = player.getStackInHand(hand);
-		if (stack.isEmpty()) {
-			return ActionResult.PASS;
-		}
-		CrucibleEntity entity = (CrucibleEntity) world.getBlockEntity(pos);
-		int level = entity.getLevel();
-		Item item = stack.getItem();
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        ItemStack stack = player.getStackInHand(hand);
+        if (stack.isEmpty()) {
+            return ActionResult.PASS;
+        }
+        CrucibleEntity entity = (CrucibleEntity) world.getBlockEntity(pos);
+        int level = entity.getLevel();
+        Item item = stack.getItem();
 
-		if (item == ModItems.BOTTLE_OF_TEARS) {
-			if (level <= 750 && !world.isClient) {
-				if (!player.abilities.creativeMode) {
-					ItemStack bottle = new ItemStack(Items.GLASS_BOTTLE);
-					stack.decrement(1);
-					player.inventory.offerOrDrop(world, bottle);
-				}
+        if (item == ModItems.BOTTLE_OF_TEARS) {
+            if (level <= 750 && !world.isClient) {
+                if (!player.abilities.creativeMode) {
+                    ItemStack bottle = new ItemStack(Items.GLASS_BOTTLE);
+                    stack.decrement(1);
+                    player.inventory.offerOrDrop(world, bottle);
+                }
 
-				world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
-				entity.setLevel(level + 250);
-			}
+                world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                entity.setLevel(level + 250);
+            }
 
-			return ActionResult.success(world.isClient);
-		}
+            return ActionResult.success(world.isClient);
+        } else {
+            ItemStack itemStack4;
+            if (item == Items.GLASS_BOTTLE) {
+                if (level > 0 && !world.isClient) {
+                    if (!player.abilities.creativeMode) {
+                        itemStack4 = new ItemStack(ModItems.BOTTLE_OF_TEARS);
+                        player.incrementStat(Stats.USE_CAULDRON);
+                        stack.decrement(1);
+                        if (stack.isEmpty()) {
+                            player.setStackInHand(hand, itemStack4);
+                        } else if (!player.inventory.insertStack(itemStack4)) {
+                            player.dropItem(itemStack4, false);
+                        } else if (player instanceof ServerPlayerEntity) {
+                            ((ServerPlayerEntity) player).openHandledScreen(player.playerScreenHandler);
+                        }
+                    }
 
-		return ActionResult.PASS;
-	}
+                    world.playSound((PlayerEntity) null, pos, SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    entity.setLevel(level - 250);
+                }
 
+                return ActionResult.success(world.isClient);
+            }
+
+            return ActionResult.PASS;
+        }
+
+    }
 }
