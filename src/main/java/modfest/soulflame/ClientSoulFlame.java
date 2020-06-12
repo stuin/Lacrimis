d0ef16148e044e86427aa1ec1510a79790fd41f2
Kunai <lunarteclipse@gmail.com>
2020-06-12 06:51:32 +0100

    cleaned up imports, added client environment to ClientSoulFlame, changed screenfactory to lambda too

--- src/main/java/modfest/soulflame/ClientSoulFlame.java
+++ src/main/java/modfest/soulflame/ClientSoulFlame.java
@@ -4,6 +4,8 @@ import modfest.soulflame.block.ModBlocks;
 import modfest.soulflame.infusion.InfusionScreen;
 import modfest.soulflame.infusion.InfusionScreenHandler;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.screen.ContainerScreenFactory;
 import net.fabricmc.fabric.api.client.screen.ScreenProviderRegistry;
@@ -13,15 +15,11 @@ import net.minecraft.client.render.RenderLayer;
 import net.minecraft.text.TranslatableText;
 
 public class ClientSoulFlame implements ClientModInitializer {
+	@Environment(EnvType.CLIENT)
 	@Override
 	public void onInitializeClient() {
 		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.infusionTable, RenderLayer.getCutout());
 
-		ScreenProviderRegistry.INSTANCE.registerFactory(SoulFlame.INFUSION_SCREEN_ID, new ContainerScreenFactory<InfusionScreenHandler>() {
-			@Override
-			public HandledScreen create(InfusionScreenHandler container) {
-				return new InfusionScreen(container, MinecraftClient.getInstance().player.inventory, new TranslatableText("gui.soulflame.infusion"));
-			}
-		});
+		ScreenProviderRegistry.INSTANCE.registerFactory(SoulFlame.INFUSION_SCREEN_ID, (ContainerScreenFactory<InfusionScreenHandler>) container -> new InfusionScreen(container, MinecraftClient.getInstance().player.inventory, new TranslatableText("gui.soulflame.infusion")));
 	}
 }
--- src/main/java/modfest/soulflame/infusion/InfusionScreenHandler.java
+++ src/main/java/modfest/soulflame/infusion/InfusionScreenHandler.java
@@ -1,10 +1,8 @@
 package modfest.soulflame.infusion;
 
-import modfest.soulflame.SoulFlame;
 import modfest.soulflame.block.ModBlocks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.CraftingInventory;
