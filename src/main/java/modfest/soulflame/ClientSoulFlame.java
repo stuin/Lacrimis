df09f8428d4a358fa76df658cd85d539f27d5038
Bohdan <reoseah9@gmail.com>
2020-06-12 06:51:29 +0200

    Make infusion gui work

--- src/main/java/modfest/soulflame/ClientSoulFlame.java
+++ src/main/java/modfest/soulflame/ClientSoulFlame.java
@@ -1,17 +1,23 @@
 package modfest.soulflame;
 
 import modfest.soulflame.infusion.InfusionScreen;
-import modfest.soulflame.infusion.RequiredTypes;
+import modfest.soulflame.infusion.InfusionScreenHandler;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.fabricmc.fabric.api.client.screen.ContainerScreenFactory;
+import net.fabricmc.fabric.api.client.screen.ScreenProviderRegistry;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.text.TranslatableText;
 
 public class ClientSoulFlame implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
+	@Override
+	public void onInitializeClient() {
+		ScreenProviderRegistry.INSTANCE.registerFactory(SoulFlame.INFUSION_SCREEN_ID, new ContainerScreenFactory<InfusionScreenHandler>() {
+			@Override
+			public HandledScreen create(InfusionScreenHandler container) {
+				return new InfusionScreen(container, MinecraftClient.getInstance().player.inventory, new TranslatableText("gui.soulflame.infusion"));
+			}
+		});
+	}
 
-    }
-
-    static {
-        HandledScreens.register(RequiredTypes.infusionScreen, InfusionScreen::new);
-    }
 }
--- src/main/java/modfest/soulflame/infusion/InfusionScreenHandler.java
+++ src/main/java/modfest/soulflame/infusion/InfusionScreenHandler.java
@@ -1,6 +1,7 @@
 package modfest.soulflame.infusion;
 
 import modfest.soulflame.SoulFlame;
+import modfest.soulflame.block.ModBlocks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.Blocks;
@@ -22,157 +23,159 @@ import net.minecraft.world.World;
 
 //Mostly copied from CraftingScreenHandler
 public class InfusionScreenHandler extends AbstractRecipeScreenHandler<CraftingInventory> {
-    private final CraftingInventory input;
-    private final CraftingResultInventory result;
-    private final ScreenHandlerContext context;
-    private final PlayerEntity player;
+	private final CraftingInventory input;
+	private final CraftingResultInventory result;
+	private final ScreenHandlerContext context;
+	private final PlayerEntity player;
 
-    public InfusionScreenHandler(int syncId, PlayerInventory playerInventory) {
-        this(syncId, playerInventory, ScreenHandlerContext.EMPTY);
-    }
+	public InfusionScreenHandler(int syncId, PlayerInventory playerInventory) {
+		this(syncId, playerInventory, ScreenHandlerContext.EMPTY);
+	}
 
-    public InfusionScreenHandler(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
-        super(RequiredTypes.infusionScreen, syncId);
-        this.input = new CraftingInventory(this, 3, 3);
-        this.result = new CraftingResultInventory();
-        this.context = context;
-        this.player = playerInventory.player;
-        this.addSlot(new CraftingResultSlot(playerInventory.player, this.input, this.result, 0, 124, 35));
+	public InfusionScreenHandler(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
+		super(null, syncId);
+		this.input = new CraftingInventory(this, 3, 3);
+		this.result = new CraftingResultInventory();
+		this.context = context;
+		this.player = playerInventory.player;
+		this.addSlot(new CraftingResultSlot(playerInventory.player, this.input, this.result, 0, 124, 35));
 
-        int m;
-        int l;
-        for(m = 0; m < 3; ++m) {
-            for(l = 0; l < 3; ++l) {
-                this.addSlot(new Slot(this.input, l + m * 3, 30 + l * 18, 17 + m * 18));
-            }
-        }
+		int m;
+		int l;
+		for (m = 0; m < 3; ++m) {
+			for (l = 0; l < 3; ++l) {
+				this.addSlot(new Slot(this.input, l + m * 3, 30 + l * 18, 17 + m * 18));
+			}
+		}
 
-        for(m = 0; m < 3; ++m) {
-            for(l = 0; l < 9; ++l) {
-                this.addSlot(new Slot(playerInventory, l + m * 9 + 9, 8 + l * 18, 84 + m * 18));
-            }
-        }
+		for (m = 0; m < 3; ++m) {
+			for (l = 0; l < 9; ++l) {
+				this.addSlot(new Slot(playerInventory, l + m * 9 + 9, 8 + l * 18, 84 + m * 18));
+			}
+		}
 
-        for(m = 0; m < 9; ++m) {
-            this.addSlot(new Slot(playerInventory, m, 8 + m * 18, 142));
-        }
+		for (m = 0; m < 9; ++m) {
+			this.addSlot(new Slot(playerInventory, m, 8 + m * 18, 142));
+		}
 
-    }
+	}
 
-    protected static void updateResult(int syncId, World world, PlayerEntity player, CraftingInventory craftingInventory, CraftingResultInventory resultInventory) {
-        if (!world.isClient) {
-            ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)player;
-            ItemStack itemStack = ItemStack.EMPTY;
-            /*Optional<CraftingRecipe> optional = world.getServer().getRecipeManager()
-                    .getFirstMatch(RecipeType.CRAFTING, craftingInventory, world);
-            if (optional.isPresent()) {
-                CraftingRecipe craftingRecipe = (CraftingRecipe)optional.get();
-                if (resultInventory.shouldCraftRecipe(world, serverPlayerEntity, craftingRecipe)) {
-                    itemStack = craftingRecipe.craft(craftingInventory);
-                }
-            }*/
+	protected static void updateResult(int syncId, World world, PlayerEntity player, CraftingInventory craftingInventory, CraftingResultInventory resultInventory) {
+		if (!world.isClient) {
+			ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) player;
+			ItemStack itemStack = ItemStack.EMPTY;
+			/*
+			 * Optional<CraftingRecipe> optional = world.getServer().getRecipeManager()
+			 * .getFirstMatch(RecipeType.CRAFTING, craftingInventory, world);
+			 * if (optional.isPresent()) {
+			 * CraftingRecipe craftingRecipe = (CraftingRecipe)optional.get();
+			 * if (resultInventory.shouldCraftRecipe(world, serverPlayerEntity, craftingRecipe)) {
+			 * itemStack = craftingRecipe.craft(craftingInventory);
+			 * }
+			 * }
+			 */
 
-            resultInventory.setStack(0, itemStack);
-            serverPlayerEntity.networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(syncId, 0, itemStack));
-        }
-    }
+			resultInventory.setStack(0, itemStack);
+			serverPlayerEntity.networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(syncId, 0, itemStack));
+		}
+	}
 
-    public void onContentChanged(Inventory inventory) {
-        this.context.run((world, blockPos) -> {
-            updateResult(this.syncId, world, this.player, this.input, this.result);
-        });
-    }
+	public void onContentChanged(Inventory inventory) {
+		this.context.run((world, blockPos) -> {
+			updateResult(this.syncId, world, this.player, this.input, this.result);
+		});
+	}
 
-    public void populateRecipeFinder(RecipeFinder finder) {
-        this.input.provideRecipeInputs(finder);
-    }
+	public void populateRecipeFinder(RecipeFinder finder) {
+		this.input.provideRecipeInputs(finder);
+	}
 
-    public void clearCraftingSlots() {
-        this.input.clear();
-        this.result.clear();
-    }
+	public void clearCraftingSlots() {
+		this.input.clear();
+		this.result.clear();
+	}
 
-    public boolean matches(Recipe<? super CraftingInventory> recipe) {
-        return recipe.matches(this.input, this.player.world);
-    }
+	public boolean matches(Recipe<? super CraftingInventory> recipe) {
+		return recipe.matches(this.input, this.player.world);
+	}
 
-    public void close(PlayerEntity player) {
-        super.close(player);
-        this.context.run((world, blockPos) -> {
-            this.dropInventory(player, world, this.input);
-        });
-    }
+	public void close(PlayerEntity player) {
+		super.close(player);
+		this.context.run((world, blockPos) -> {
+			this.dropInventory(player, world, this.input);
+		});
+	}
 
-    public boolean canUse(PlayerEntity player) {
-        return canUse(this.context, player, Blocks.CRAFTING_TABLE);
-    }
+	public boolean canUse(PlayerEntity player) {
+		return canUse(this.context, player, ModBlocks.infusionTable);
+	}
 
-    public ItemStack transferSlot(PlayerEntity player, int index) {
-        ItemStack itemStack = ItemStack.EMPTY;
-        Slot slot = (Slot)this.slots.get(index);
-        if (slot != null && slot.hasStack()) {
-            ItemStack itemStack2 = slot.getStack();
-            itemStack = itemStack2.copy();
-            if (index == 0) {
-                this.context.run((world, blockPos) -> {
-                    itemStack2.getItem().onCraft(itemStack2, world, player);
-                });
-                if (!this.insertItem(itemStack2, 10, 46, true)) {
-                    return ItemStack.EMPTY;
-                }
+	public ItemStack transferSlot(PlayerEntity player, int index) {
+		ItemStack itemStack = ItemStack.EMPTY;
+		Slot slot = (Slot) this.slots.get(index);
+		if (slot != null && slot.hasStack()) {
+			ItemStack itemStack2 = slot.getStack();
+			itemStack = itemStack2.copy();
+			if (index == 0) {
+				this.context.run((world, blockPos) -> {
+					itemStack2.getItem().onCraft(itemStack2, world, player);
+				});
+				if (!this.insertItem(itemStack2, 10, 46, true)) {
+					return ItemStack.EMPTY;
+				}
 
-                slot.onStackChanged(itemStack2, itemStack);
-            } else if (index >= 10 && index < 46) {
-                if (!this.insertItem(itemStack2, 1, 10, false)) {
-                    if (index < 37) {
-                        if (!this.insertItem(itemStack2, 37, 46, false)) {
-                            return ItemStack.EMPTY;
-                        }
-                    } else if (!this.insertItem(itemStack2, 10, 37, false)) {
-                        return ItemStack.EMPTY;
-                    }
-                }
-            } else if (!this.insertItem(itemStack2, 10, 46, false)) {
-                return ItemStack.EMPTY;
-            }
+				slot.onStackChanged(itemStack2, itemStack);
+			} else if (index >= 10 && index < 46) {
+				if (!this.insertItem(itemStack2, 1, 10, false)) {
+					if (index < 37) {
+						if (!this.insertItem(itemStack2, 37, 46, false)) {
+							return ItemStack.EMPTY;
+						}
+					} else if (!this.insertItem(itemStack2, 10, 37, false)) {
+						return ItemStack.EMPTY;
+					}
+				}
+			} else if (!this.insertItem(itemStack2, 10, 46, false)) {
+				return ItemStack.EMPTY;
+			}
 
-            if (itemStack2.isEmpty()) {
-                slot.setStack(ItemStack.EMPTY);
-            } else {
-                slot.markDirty();
-            }
+			if (itemStack2.isEmpty()) {
+				slot.setStack(ItemStack.EMPTY);
+			} else {
+				slot.markDirty();
+			}
 
-            if (itemStack2.getCount() == itemStack.getCount()) {
-                return ItemStack.EMPTY;
-            }
+			if (itemStack2.getCount() == itemStack.getCount()) {
+				return ItemStack.EMPTY;
+			}
 
-            ItemStack itemStack3 = slot.onTakeItem(player, itemStack2);
-            if (index == 0) {
-                player.dropItem(itemStack3, false);
-            }
-        }
+			ItemStack itemStack3 = slot.onTakeItem(player, itemStack2);
+			if (index == 0) {
+				player.dropItem(itemStack3, false);
+			}
+		}
 
-        return itemStack;
-    }
+		return itemStack;
+	}
 
-    public boolean canInsertIntoSlot(ItemStack stack, Slot slot) {
-        return slot.inventory != this.result && super.canInsertIntoSlot(stack, slot);
-    }
+	public boolean canInsertIntoSlot(ItemStack stack, Slot slot) {
+		return slot.inventory != this.result && super.canInsertIntoSlot(stack, slot);
+	}
 
-    public int getCraftingResultSlotIndex() {
-        return 0;
-    }
+	public int getCraftingResultSlotIndex() {
+		return 0;
+	}
 
-    public int getCraftingWidth() {
-        return this.input.getWidth();
-    }
+	public int getCraftingWidth() {
+		return this.input.getWidth();
+	}
 
-    public int getCraftingHeight() {
-        return this.input.getHeight();
-    }
+	public int getCraftingHeight() {
+		return this.input.getHeight();
+	}
 
-    @Environment(EnvType.CLIENT)
-    public int getCraftingSlotCount() {
-        return 10;
-    }
+	@Environment(EnvType.CLIENT)
+	public int getCraftingSlotCount() {
+		return 10;
+	}
 }
