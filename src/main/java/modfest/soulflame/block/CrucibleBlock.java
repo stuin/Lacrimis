2cd8d4423dc0158d0ea6a1c9aa32627bef7dab46
Bohdan <reoseah9@gmail.com>
2020-06-12 14:15:55 +0200

    Remove unused imports and add loot table for crucible

--- src/main/java/modfest/soulflame/block/CrucibleBlock.java
+++ src/main/java/modfest/soulflame/block/CrucibleBlock.java
@@ -11,8 +11,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.potion.PotionUtil;
-import net.minecraft.potion.Potions;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
