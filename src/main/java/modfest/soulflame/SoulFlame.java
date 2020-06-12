5fe1525f40fad4b99933163cc6b1dec6df33eb6d
Stuart Irwin <stuin01@gmail.com>
2020-06-11 17:23:14 -0700

    Set modid soulflame

---
+++ src/main/java/modfest/soulflame/SoulFlame.java
@@ -0,0 +1,11 @@
+package modfest.soulflame;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SoulFlame implements ModInitializer {
+	public static final String MODID = "soulflame";
+	
+	@Override
+	public void onInitialize() {
+	}
+}
