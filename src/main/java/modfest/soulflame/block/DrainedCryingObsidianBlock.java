48d995efe63785b71917f75df578d49baaeb7579
2xsaiko <git@dblsaiko.net>
2020-06-14 22:15:09 +0200

    Work around properties bug and add textures for drained crying obsidian

--- src/main/java/modfest/soulflame/block/DrainedCryingObsidianBlock.java
+++ src/main/java/modfest/soulflame/block/DrainedCryingObsidianBlock.java
@@ -2,6 +2,7 @@ package modfest.soulflame.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.CryingObsidianBlock;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.state.StateManager.Builder;
@@ -14,18 +15,22 @@ import net.fabricmc.api.Environment;
 
 import java.util.Random;
 
+import modfest.soulflame.init.ModBlocks;
+
 public class DrainedCryingObsidianBlock extends CryingObsidianBlock {
 
-    public static final IntProperty REMAINING_TEARS = IntProperty.of("tears", 1, 499);
+    public static final IntProperty TEARS_LEAST = IntProperty.of("tears_least_sig", 0, 49);
+    public static final IntProperty TEARS_MOST = IntProperty.of("tears_most_sig", 0, 9);
 
     public DrainedCryingObsidianBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.getDefaultState().with(REMAINING_TEARS, 499));
+        this.setDefaultState(setTearsValue(this.getDefaultState(), 499));
     }
 
     @Override
     protected void appendProperties(Builder<Block, BlockState> builder) {
-        builder.add(REMAINING_TEARS);
+        builder.add(TEARS_LEAST);
+        builder.add(TEARS_MOST);
     }
 
     @Environment(EnvType.CLIENT)
@@ -43,6 +48,32 @@ public class DrainedCryingObsidianBlock extends CryingObsidianBlock {
                 }
             }
         }
+    }
+
+    public static BlockState setTearsValue(BlockState self, int tears) {
+        int least = tears % 50;
+        int most = tears / 50;
+        return self.with(TEARS_LEAST, least).with(TEARS_MOST, most);
+    }
+
+    public static int getTearsValue(BlockState self) {
+        return self.get(TEARS_MOST) * 50 + self.get(TEARS_LEAST);
+    }
+
+    public static BlockState getStateForTearsLevel(int tears) {
+        if (tears == 501) {
+            return Blocks.CRYING_OBSIDIAN.getDefaultState();
+        } else if (tears == 0) {
+            return Blocks.OBSIDIAN.getDefaultState();
+        } else {
+            return setTearsValue(ModBlocks.drainedCryingObsidian.getDefaultState(), tears - 1);
+        }
+    }
+
+    public static int getTearsLevel(BlockState self) {
+        if (self.getBlock() == Blocks.OBSIDIAN) return 0;
+        else if (self.getBlock() == Blocks.CRYING_OBSIDIAN) return 501;
+        else return getTearsValue(self) + 1;
     }
 
 }
