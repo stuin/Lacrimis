577c442593ab4f4150f2726c78afea55f735ac92
Stuart Irwin <stuin01@gmail.com>
2020-07-15 13:23:13 -0700

    Adjusted tear lantern efficiency

--- src/main/java/modfest/lacrimis/block/DrainedCryingObsidianBlock.java
+++ src/main/java/modfest/lacrimis/block/DrainedCryingObsidianBlock.java
@@ -39,24 +39,6 @@ public class DrainedCryingObsidianBlock extends CryingObsidianBlock {
         return PistonBehavior.BLOCK;
     }
 
-    @Override
-    @Environment(EnvType.CLIENT)
-    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
-        if (random.nextInt(5) == 0) {
-            Direction direction = Direction.random(random);
-            if (direction != Direction.UP) {
-                BlockPos blockPos = pos.offset(direction);
-                BlockState blockState = world.getBlockState(blockPos);
-                if (!state.isOpaque() || !blockState.isSideSolidFullSquare(world, blockPos, direction.getOpposite())) {
-                    double d = direction.getOffsetX() == 0 ? random.nextDouble() : 0.5 + direction.getOffsetX() * 0.6;
-                    double e = direction.getOffsetY() == 0 ? random.nextDouble() : 0.5 + direction.getOffsetY() * 0.6;
-                    double f = direction.getOffsetZ() == 0 ? random.nextDouble() : 0.5 + direction.getOffsetZ() * 0.6;
-                    world.addParticle(ParticleTypes.DRIPPING_OBSIDIAN_TEAR, pos.getX() + d, pos.getY() + e, pos.getZ() + f, 0.0D, 0.0D, 0.0D);
-                }
-            }
-        }
-    }
-
     public static BlockState setTearsValue(BlockState self, int tears) {
         tears -= 1;
         int least = tears % 50;
--- src/main/java/modfest/lacrimis/block/entity/TearLanternEntity.java
+++ src/main/java/modfest/lacrimis/block/entity/TearLanternEntity.java
@@ -18,38 +18,50 @@ public class TearLanternEntity extends BlockEntity implements Tickable {
         super(ModBlockEntityTypes.tearLantern);
     }
 
-
     @Override
     public void tick() {
         int range = 6;
         BlockPos obsidianPos = this.pos.up(1);
-        if (world != null && this.world.getTime() % 3 == 0) {
+        if (world != null && this.world.getTime() % 5 == 0) {
+            //Display dripping particles
             if (world.isClient && Math.random() > 0.9)
                 world.addParticle(ParticleTypes.DRIPPING_OBSIDIAN_TEAR, pos.getX() + (0.4 + Math.random() * (0.6 - 0.4)), pos.getY() - 0.1D, pos.getZ() + (0.4 + Math.random() * (0.6 - 0.4)), 0.0D, 0.0D, 0.0D);
+
+            //Look for tear target
             BlockState obsidianState = this.world.getBlockState(obsidianPos);
-            if (obsidianState.getBlock() == ModBlocks.drainedCryingObsidian || obsidianState.getBlock() == Blocks.CRYING_OBSIDIAN) {
-                int count = 0;
+            int remains = DrainedCryingObsidianBlock.getTearsLevel(obsidianState);
+            if (remains > 0) {
                 for (int x = -range; x <= range; x++)
                     for (int y = -range; y <= range; y++)
                         for (int z = -range; z <= range; z++) {
                             BlockEntity table = world.getBlockEntity(pos.add(x, y - 1, z));
                             if (table instanceof CrucibleEntity) {
                                 CrucibleEntity casted = (CrucibleEntity) table;
-                                if (casted.getTank().getSpace() > 0) {
-                                    if (world.isClient) {
-                                        double xrand = Math.random() * 0.2 - 0.1;
-                                        double yrand = Math.random() * 0.2 - 0.1;
-                                        double zrand = Math.random() * 0.2 - 0.1;
+                                int space = casted.getTank().getSpace();
+                                //Display particle stream
+                                if(space > 0 && world.isClient) {
+                                    double xrand = Math.random() * 0.2 - 0.1;
+                                    double yrand = Math.random() * 0.2 - 0.1;
+                                    double zrand = Math.random() * 0.2 - 0.1;
 
-                                        world.addParticle(ModParticles.OBSIDIAN_TEAR_FLYING, this.pos.getX() + 0.5 + xrand, this.pos.getY() + 0.2 + yrand, this.pos.getZ() + 0.5 + zrand, x * 0.02, y * 0.02, z * 0.02);
-                                    }
-                                    count++;
-                                    casted.getTank().addTears(2);
+                                    world.addParticle(ModParticles.OBSIDIAN_TEAR_FLYING,
+                                            this.pos.getX() + 0.5 + xrand, this.pos.getY() + 0.2 + yrand, this.pos.getZ() + 0.5 + zrand,
+                                            x * 0.02, y * 0.02, z * 0.02);
+                                }
+
+                                //Move tears
+                                if(remains >= 3 && casted.getTank().getSpace() >= 4) {
+                                    casted.getTank().addTears(4);
+                                    world.setBlockState(obsidianPos, DrainedCryingObsidianBlock.removeTearState(obsidianState, 3));
+                                    return;
+                                } else if(space > 0) {
+                                    remains = Math.min(space, remains);
+                                    casted.getTank().addTears(remains);
+                                    world.setBlockState(obsidianPos, DrainedCryingObsidianBlock.removeTearState(obsidianState, remains));
+                                    return;
                                 }
                             }
                         }
-                if(count > 0)
-                    world.setBlockState(obsidianPos, DrainedCryingObsidianBlock.removeTearState(obsidianState, count * 2));
             }
         }
     }
