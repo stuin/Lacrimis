e19089bf7ad8a98a2f0fdb608bfba084ee59890c
Stuart Irwin <stuin01@gmail.com>
2021-06-12 16:26:44 -0700

    Readded entity textures and adjusted shell renderer

--- src/main/java/modfest/lacrimis/client/render/entity/SoulShellModel.java
+++ src/main/java/modfest/lacrimis/client/render/entity/SoulShellModel.java
@@ -1,8 +1,5 @@
 package modfest.lacrimis.client.render.entity;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.model.ModelPart;
@@ -12,80 +9,21 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Arm;
 
-import java.util.List;
-import java.util.Random;
-
 @Environment(EnvType.CLIENT)
 public class SoulShellModel<T extends LivingEntity> extends BipedEntityModel<T> {
-    private List<ModelPart> parts = Lists.newArrayList();
-    public final ModelPart leftSleeve;
-    public final ModelPart rightSleeve;
-    public final ModelPart leftPantLeg;
-    public final ModelPart rightPantLeg;
-    public final ModelPart jacket;
-
     public SoulShellModel(float scale) {
         super(RenderLayer::getEntityTranslucent, scale, 0.0F, 64, 64);
         this.leftArm = new ModelPart(this, 32, 48);
         this.leftArm.addCuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, scale);
         this.leftArm.setPivot(5.0F, 2.0F, 0.0F);
-        this.leftSleeve = new ModelPart(this, 48, 48);
-        this.leftSleeve.addCuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, scale + 0.25F);
-        this.leftSleeve.setPivot(5.0F, 2.0F, 0.0F);
-        this.rightSleeve = new ModelPart(this, 40, 32);
-        this.rightSleeve.addCuboid(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, scale + 0.25F);
-        this.rightSleeve.setPivot(-5.0F, 2.0F, 10.0F);
         this.leftLeg = new ModelPart(this, 16, 48);
         this.leftLeg.addCuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, scale);
         this.leftLeg.setPivot(1.9F, 12.0F, 0.0F);
-        this.leftPantLeg = new ModelPart(this, 0, 48);
-        this.leftPantLeg.addCuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, scale + 0.25F);
-        this.leftPantLeg.setPivot(1.9F, 12.0F, 0.0F);
-        this.rightPantLeg = new ModelPart(this, 0, 32);
-        this.rightPantLeg.addCuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, scale + 0.25F);
-        this.rightPantLeg.setPivot(-1.9F, 12.0F, 0.0F);
-        this.jacket = new ModelPart(this, 16, 32);
-        this.jacket.addCuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, scale + 0.25F);
-        this.jacket.setPivot(0.0F, 0.0F, 0.0F);
-    }
-
-    protected Iterable<ModelPart> getBodyParts() {
-        return Iterables.concat(super.getBodyParts(), ImmutableList.of(this.leftPantLeg, this.rightPantLeg, this.leftSleeve, this.rightSleeve, this.jacket));
-    }
-
-    public void setAngles(T livingEntity, float f, float g, float h, float i, float j) {
-        super.setAngles(livingEntity, f, g, h, i, j);
-        this.leftPantLeg.copyPositionAndRotation(this.leftLeg);
-        this.rightPantLeg.copyPositionAndRotation(this.rightLeg);
-        this.leftSleeve.copyPositionAndRotation(this.leftArm);
-        this.rightSleeve.copyPositionAndRotation(this.rightArm);
-        this.jacket.copyPositionAndRotation(this.torso);
-    }
-
-    public void setVisible(boolean visible) {
-        super.setVisible(visible);
-        this.leftSleeve.visible = visible;
-        this.rightSleeve.visible = visible;
-        this.leftPantLeg.visible = visible;
-        this.rightPantLeg.visible = visible;
-        this.jacket.visible = visible;
     }
 
     public void setArmAngle(Arm arm, MatrixStack matrices) {
         ModelPart modelPart = this.getArm(arm);
         modelPart.rotate(matrices);
-    }
-
-    public ModelPart getRandomPart(Random random) {
-        return this.parts.get(random.nextInt(this.parts.size()));
-    }
-
-    public void accept(ModelPart modelPart) {
-        if (this.parts == null) {
-            this.parts = Lists.newArrayList();
-        }
-
-        this.parts.add(modelPart);
     }
 }
 
--- src/main/java/modfest/lacrimis/client/render/entity/SoulShellRenderer.java
+++ src/main/java/modfest/lacrimis/client/render/entity/SoulShellRenderer.java
@@ -24,14 +24,14 @@ import net.minecraft.util.math.Vec3d;
 
 @Environment(EnvType.CLIENT)
 public class SoulShellRenderer extends LivingEntityRenderer<SoulShellEntity, SoulShellModel<SoulShellEntity>> {
-    public static final Identifier TEXTURE = new Identifier(Lacrimis.MODID, "textures/entity/ghost.png");
+    public static final Identifier TEXTURE = new Identifier(Lacrimis.MODID, "textures/entity/soul_shel.png");
 
     public SoulShellRenderer(EntityRenderDispatcher dispatcher) {
-        super(dispatcher, new SoulShellModel(0.0F), 0.5F);
-        this.addFeature(new ArmorFeatureRenderer(this, new BipedEntityModel(0.5F), new BipedEntityModel(1.0F)));
-        this.addFeature(new HeldItemFeatureRenderer(this));
-        this.addFeature(new HeadFeatureRenderer(this));
-        this.addFeature(new ElytraFeatureRenderer(this));
+        super(dispatcher, new SoulShellModel<>(0.0F), 0.5F);
+        this.addFeature(new ArmorFeatureRenderer(this, new BipedEntityModel<SoulShellEntity>(0.5F), new BipedEntityModel<SoulShellEntity>(1.0F)));
+        this.addFeature(new HeldItemFeatureRenderer<>(this));
+        //this.addFeature(new HeadFeatureRenderer<>(this));
+        this.addFeature(new ElytraFeatureRenderer<>(this));
     }
 
     public void render(SoulShellEntity abstractClientPlayerEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
@@ -45,25 +45,19 @@ public class SoulShellRenderer extends LivingEntityRenderer<SoulShellEntity, Sou
 
     private void setModelPose(SoulShellEntity abstractClientPlayerEntity) {
         SoulShellModel<SoulShellEntity> playerEntityModel = this.getModel();
-        if (abstractClientPlayerEntity.isSpectator()) {
-            playerEntityModel.setVisible(false);
-            playerEntityModel.head.visible = true;
-            playerEntityModel.helmet.visible = true;
-        } else {
-            playerEntityModel.setVisible(true);
-            BipedEntityModel.ArmPose armPose = getArmPose(abstractClientPlayerEntity, Hand.MAIN_HAND);
-            BipedEntityModel.ArmPose armPose2 = getArmPose(abstractClientPlayerEntity, Hand.OFF_HAND);
-            if (armPose.method_30156()) {
-                armPose2 = abstractClientPlayerEntity.getOffHandStack().isEmpty() ? BipedEntityModel.ArmPose.EMPTY : BipedEntityModel.ArmPose.ITEM;
-            }
+        playerEntityModel.setVisible(true);
+        BipedEntityModel.ArmPose armPose = getArmPose(abstractClientPlayerEntity, Hand.MAIN_HAND);
+        BipedEntityModel.ArmPose armPose2 = getArmPose(abstractClientPlayerEntity, Hand.OFF_HAND);
+        if (armPose.method_30156()) {
+            armPose2 = abstractClientPlayerEntity.getOffHandStack().isEmpty() ? BipedEntityModel.ArmPose.EMPTY : BipedEntityModel.ArmPose.ITEM;
+        }
 
-            if (abstractClientPlayerEntity.getMainArm() == Arm.RIGHT) {
-                playerEntityModel.rightArmPose = armPose;
-                playerEntityModel.leftArmPose = armPose2;
-            } else {
-                playerEntityModel.rightArmPose = armPose2;
-                playerEntityModel.leftArmPose = armPose;
-            }
+        if (abstractClientPlayerEntity.getMainArm() == Arm.RIGHT) {
+            playerEntityModel.rightArmPose = armPose;
+            playerEntityModel.leftArmPose = armPose2;
+        } else {
+            playerEntityModel.rightArmPose = armPose2;
+            playerEntityModel.leftArmPose = armPose;
         }
 
     }
@@ -85,37 +79,14 @@ public class SoulShellRenderer extends LivingEntityRenderer<SoulShellEntity, Sou
         matrixStack.scale(0.9375F, 0.9375F, 0.9375F);
     }
 
-    public void renderRightArm(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, SoulShellEntity player) {
-        this.renderArm(matrices, vertexConsumers, light, player, model.rightArm, model.rightSleeve);
-    }
-
-    public void renderLeftArm(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, SoulShellEntity player) {
-        this.renderArm(matrices, vertexConsumers, light, player, model.leftArm, model.leftSleeve);
-    }
-
-    private void renderArm(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, SoulShellEntity player, ModelPart arm, ModelPart sleeve) {
-        this.setModelPose(player);
-        model.handSwingProgress = 0.0F;
-        model.sneaking = false;
-        model.leaningPitch = 0.0F;
-        model.setAngles(player, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
-        arm.pitch = 0.0F;
-        arm.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySolid(TEXTURE)), light, OverlayTexture.DEFAULT_UV);
-        sleeve.pitch = 0.0F;
-        sleeve.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(TEXTURE)), light, OverlayTexture.DEFAULT_UV);
-    }
-
     protected void setupTransforms(SoulShellEntity abstractClientPlayerEntity, MatrixStack matrixStack, float f, float g, float h) {
-        float i = abstractClientPlayerEntity.getLeaningPitch(h);
         float n;
         float k;
         if (abstractClientPlayerEntity.isFallFlying()) {
             super.setupTransforms(abstractClientPlayerEntity, matrixStack, f, g, h);
             n = (float)abstractClientPlayerEntity.getRoll() + h;
             k = MathHelper.clamp(n * n / 100.0F, 0.0F, 1.0F);
-            if (!abstractClientPlayerEntity.isUsingRiptide()) {
-                matrixStack.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(k * (-90.0F - abstractClientPlayerEntity.pitch)));
-            }
+            matrixStack.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(k * (-90.0F - abstractClientPlayerEntity.pitch)));
 
             Vec3d vec3d = abstractClientPlayerEntity.getRotationVec(h);
             Vec3d vec3d2 = abstractClientPlayerEntity.getVelocity();
@@ -125,14 +96,6 @@ public class SoulShellRenderer extends LivingEntityRenderer<SoulShellEntity, Sou
                 double l = (vec3d2.x * vec3d.x + vec3d2.z * vec3d.z) / Math.sqrt(d * e);
                 double m = vec3d2.x * vec3d.z - vec3d2.z * vec3d.x;
                 matrixStack.multiply(Vector3f.POSITIVE_Y.getRadialQuaternion((float)(Math.signum(m) * Math.acos(l))));
-            }
-        } else if (i > 0.0F) {
-            super.setupTransforms(abstractClientPlayerEntity, matrixStack, f, g, h);
-            n = abstractClientPlayerEntity.isTouchingWater() ? -90.0F - abstractClientPlayerEntity.pitch : -90.0F;
-            k = MathHelper.lerp(i, 0.0F, n);
-            matrixStack.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(k));
-            if (abstractClientPlayerEntity.isInSwimmingPose()) {
-                matrixStack.translate(0.0D, -1.0D, 0.30000001192092896D);
             }
         } else {
             super.setupTransforms(abstractClientPlayerEntity, matrixStack, f, g, h);
